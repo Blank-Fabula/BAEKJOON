b9c4d2faa1822176923bf5478ea041880682dfc7
LEE GI TEAK <fabula.story47@gmail.com>
2023-07-02 14:39:13 +0900

    Update 별 찍기 － 1.cc

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -1,22 +1,12 @@
 #include <iostream>
+
 using namespace std;
 
-int main(void){
-
-    int S = 0, n1 = 0, n2 = 0, n3=0;
-
-    scanf("%d", &S);
-
-    n1 = S;
-    n3 = S;
-    for (S = S; S > 0; S--) {
-        n2 = n3;
-        for (n2 = n2; n2 <= n1; n2++) {
-            printf("*"); 
-        }
-        n1++;
-        printf("\n");
-
+int main() { int num;
+    cin >> num;
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        } cout << endl;
     }
-    return 0;
-}//메인함수 끝
+return 0; }
