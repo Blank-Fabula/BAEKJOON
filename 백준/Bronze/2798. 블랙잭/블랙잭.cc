3fd83405ff019d9d339c02ac1fc1f1c6d3128839
LEE GI TEAK <fabula.story47@gmail.com>
2023-07-04 22:22:00 +0900

    Update 블랙잭.cc

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -6,8 +6,8 @@ int main() { int N, M;
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> arrNum[i];
-    }
-    int sum = 0;
+    	}
+	int sum = 0;
 	for (int i = 0; i < N; i++) {
 		for (int j = i + 1; j < N; j++) {
 			for (int k = j + 1; k < N; k++) {
