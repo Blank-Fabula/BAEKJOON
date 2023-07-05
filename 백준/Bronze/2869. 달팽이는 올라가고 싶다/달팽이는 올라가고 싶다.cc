d7e32d220a924d189f6529f169cc292911318eba
LEE GI TEAK <fabula.story47@gmail.com>
2023-07-05 12:23:47 +0900

    Update 달팽이는 올라가고 싶다.cc

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -3,5 +3,7 @@ using namespace std;
 
 int main() { int A, B, V;
 	cin >> A >> B >> V;
-	cout << (V - B - 1) / (A - B) + 1;
+	int snail = (V - A) / (A - B) + 1;
+	if ((V - A) % (A - B) != 0) snail++;
+	cout << snail;
 return 0; }
