7249dab06dc433ecab25ada7a59e8a860df058dc
LEE GI TEAK <fabula.story47@gmail.com>
2023-07-05 12:16:58 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -3,12 +3,5 @@ using namespace std;
 
 int main() { int A, B, V;
 	cin >> A >> B >> V;
-	int day;
-	if ((V - A) % (A - B) == 0) {
-		day = (V - A) / (A - B) + 1;
-	}
-	else {
-		day = (V - A) / (A - B) + 2;
-	}
-	cout << day;
+	cout << (V-B-1) / (A-B) + 1;
 return 0; }
