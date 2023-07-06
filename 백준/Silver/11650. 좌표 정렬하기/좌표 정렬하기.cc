df825e79a73c09e1977ddd14f3db51d73bdbae33
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-07 00:24:54 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 120 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() { int num;
+    cin >> num;
+    pair<int, int> coord;
+    vector<pair<int, int>> arrCoord;
+    for (int i = 0; i < num; i++) {
+        cin >> coord.first >> coord.second;
+        arrCoord.push_back(coord);
+    }
+    sort(arrCoord.begin(), arrCoord.end());
+    for (int i = 0; i < num; i++) {
+        cout << arrCoord[i].first << ' ' << arrCoord[i].second << '\n';
+    }
+return 0; }
