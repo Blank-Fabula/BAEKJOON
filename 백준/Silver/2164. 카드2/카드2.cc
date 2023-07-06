d72293d1c83c244f0e999558a61a56d5137596fa
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-06 23:58:01 +0900

    [Silver IV] Title: 카드2, Time: 4 ms, Memory: 4040 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() { int N;
+	cin >> N;
+	queue<int> q;
+	for (int i = 0; i < N; i++) {
+		q.push(i + 1);
+	}
+	while (q.size() > 1) {
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.back() << "\n";
+return 0; }
