9fc8ff59655ce2fa7d5d8634d3c1a2fb6878223f
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-09 00:22:41 +0900

    [Silver IV] Title: 큐, Time: 4 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[10000];
+int stack(string, int);
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N; cin >> N;
+    for (int size = 0; N!=0; N--) {
+        string word;
+        cin >> word;
+        size = stack(word, size);
+    }
+return 0; }
+
+int stack(string word, int size) {
+    if (word == "push") {
+        cin >> arr[size];
+        size++;
+	}
+    else if (word == "pop") {
+        if (arr[0]) {
+            cout << arr[0] << "\n";
+            for (int i = 0; i < size; i++) {
+                arr[i] = arr[i+1];
+            }
+            size--;
+        }
+        else cout << -1 << "\n";
+    }
+    else if (word == "size") {
+        cout << size << "\n";
+    }
+    else if (word == "empty") {
+        if (size) cout << 0 << "\n";
+        else cout << 1 << "\n";
+    }
+    else if (word == "front") {
+        if (arr[0])cout << arr[0] << "\n";
+        else cout << -1 << "\n";
+    }
+    else if (word == "back") {
+        if (arr[size - 1])cout << arr[size - 1] << "\n";
+        else cout << -1 << "\n";
+    }
+return size; }
