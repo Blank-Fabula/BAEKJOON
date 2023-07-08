80f74887e211a5910b73f4521f258153017b0230
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-08 22:09:12 +0900

    [Silver II] Title: 랜선 자르기, Time: 12 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int lan[10000];
+int main() { int K, N;
+	cin >> K >> N;
+	int  maxvalue = 0;
+	for (int i = 0; i < K; i++) {
+		cin >> lan[i];
+		if (lan[i] > maxvalue) maxvalue = lan[i];
+	}
+	long mid, low = 1, high = maxvalue;
+	int result = 0;
+	while (low <= high) {
+		mid = (low + high) / 2;
+		int cnt = 0;
+		for (int i = 0; i < K; i++) {
+			cnt += lan[i] / mid;
+		}
+		if (cnt >= N) {
+			low = mid + 1;
+			if (result < mid) result = mid;
+		}
+		else high = mid - 1;
+	}
+	cout << result;
+return 0; }
