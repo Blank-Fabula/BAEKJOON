b1c32c34476da7f2b40b2f8c25a7c01f53ee70ee
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-09 08:30:38 +0900

    Update 최대공약수와 최소공배수.cc

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -4,6 +4,8 @@ using namespace std;
 int gcd(int, int);
 int lcm(int, int);
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	int A, B; cin >> A >> B;
 	cout << gcd(A, B) << "\n" << lcm(A, B);
 return 0; }
