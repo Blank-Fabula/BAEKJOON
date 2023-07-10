ca4909428de07acf92b2d3df1c3db6c314a118c4
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-10 10:40:59 +0900

    Update 1로 만들기.cc

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,21 +1,19 @@
-#include <stdio.h>
-#define MIN(a,b) a<b?a:b
-int dp[1000001];
-int main()
-{
-    int num;
-    scanf("%d", &num);
-    for (int i = 2; i <= num; i++)
-    {
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    int N; cin >> N;
+    vector<int> dp(N + 1);
+    for (int i = 2; i <= N; i++) {
         dp[i] = dp[i - 1] + 1;
         if (i % 2 == 0)
-        {
-            dp[i] = (MIN(dp[i], dp[i / 2] + 1));
-        }
+            dp[i] = min(dp[i], dp[i / 2] + 1);
         if (i % 3 == 0)
-        {
-            dp[i] = (MIN(dp[i], dp[i / 3] + 1));
-        }
+            dp[i] = min(dp[i], dp[i / 3] + 1);
     }
-    printf("%d", dp[num]);
-}
+    cout << dp[N];
+return 0; }
