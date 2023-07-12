87ae2b9682f5a8a3d8c9c3b6fa3f0537b4c4cce9
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-12 22:19:03 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+#define N_MAX 40
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	pair<int, int> arrPairs[41];
+	int cntZero = 1, cntOne = 0;
+	for (int tmp, num = 0; num <= N_MAX; num++) {
+		arrPairs[num] = make_pair(cntZero, cntOne);
+		tmp = cntZero;
+		cntZero = cntOne;
+		cntOne += tmp;
+	}
+	int T; cin >> T;
+	while (T--) {
+		int N; cin >> N;
+		cout << arrPairs[N].first << " " << arrPairs[N].second << "\n";
+	}
+return 0; }
