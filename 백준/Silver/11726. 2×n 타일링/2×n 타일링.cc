b1a9c146e7ca4b46a2ddb5630a034babb1fa5c4b
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-14 03:47:32 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(int avg, char** argv) {
+	long long arr[1001] = { 1, 1 };
+	long long N; cin >> N;
+	for (int i = 2; i <= N; i++) {
+		arr[i] = (arr[i - 1] + arr[i - 2]) % 10'007;
+	}
+	cout << arr[N] << "\n";
+return 0; }
