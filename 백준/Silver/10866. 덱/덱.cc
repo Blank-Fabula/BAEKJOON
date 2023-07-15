60c91981c9ba65de61cf6b7a6585f1e53b6ece8b
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-16 03:14:08 +0900

    Update 덱.cc

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(int avg, char** argv) {
-	ios::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	deque<int> D;
 	int N; cin >> N;
 	for (int i = 0; i < N; i++) {
