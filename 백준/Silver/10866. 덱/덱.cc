b741dfd42bb2a469e87418f0a84dee96a146f78a
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-16 03:19:45 +0900

    Update 덱.cc

--- 백준/Silver/10866. 덱/덱.cc
+++ 백준/Silver/10866. 덱/덱.cc
@@ -5,41 +5,41 @@ using namespace std;
 int main(int avg, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	deque<int> D;
+	deque<int> dq;
 	int N; cin >> N;
 	for (int i = 0; i < N; i++) {
-		string op; cin >> op;
-		if (op.substr(0, 4) == "push") {
+		string word; cin >> word;
+		if (word.substr(0, 4) == "push") {
 			int X; cin >> X;
-			if (op == "push_front")
-				D.push_front(X);
-			else if (op == "push_back")
-				D.push_back(X);
+			if (word == "push_front")
+				dq.push_front(X);
+			else if (word == "push_back")
+				dq.push_back(X);
 		}
-		else if (op.substr(0, 3) == "pop") {
-			if (D.empty()) cout << -1 << '\n';
-			else if (op == "pop_front") {
-				cout << D.front() << '\n';
-				D.pop_front();
+		else if (word.substr(0, 3) == "pop") {
+			if (dq.empty()) cout << -1 << '\n';
+			else if (word == "pop_front") {
+				cout << dq.front() << '\n';
+				dq.pop_front();
 			}
-			else if (op == "pop_back") {
-				cout << D.back() << '\n';
-				D.pop_back();
+			else if (word == "pop_back") {
+				cout << dq.back() << '\n';
+				dq.pop_back();
 			}
 		}
-		else if (op == "size") {
-			cout << D.size() << '\n';
+		else if (word == "size") {
+			cout << dq.size() << '\n';
 		}
-		else if (op == "empty") {
-			cout << D.empty() << '\n';
+		else if (word == "empty") {
+			cout << dq.empty() << '\n';
 		}
-		else if (op == "front") {
-			if (D.empty()) cout << -1 << '\n';
-			else cout << D.front() << '\n';
+		else if (word == "front") {
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.front() << '\n';
 		}
-		else if (op == "back") {
-			if (D.empty()) cout << -1 << '\n';
-			else cout << D.back() << '\n';
+		else if (word == "back") {
+			if (dq.empty()) cout << -1 << '\n';
+			else cout << dq.back() << '\n';
 		}
 	}
 return 0; }
