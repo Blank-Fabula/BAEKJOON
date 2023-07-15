005704ddfe0ad1c33c0046f92a686e85292a1923
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-16 03:46:43 +0900

    [Silver IV] Title: 듣보잡, Time: 40 ms, Memory: 6172 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(int avg, char** argv) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    vector<string>v_Unheard, v_NotSeen;
+    int N, M; cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        string s; cin >> s;
+        v_Unheard.push_back(s);
+    }
+    sort(v_Unheard.begin(), v_Unheard.end());
+    for (int i = 0; i < M; i++) {
+        string s; cin >> s;
+        if (binary_search(v_Unheard.begin(), v_Unheard.end(), s)) {
+            v_NotSeen.push_back(s);
+        }
+    }
+    sort(v_NotSeen.begin(), v_NotSeen.end());
+    cout << v_NotSeen.size() << '\n';
+    for (auto result : v_NotSeen) {
+        cout << result << '\n';
+    }
+return 0; }
