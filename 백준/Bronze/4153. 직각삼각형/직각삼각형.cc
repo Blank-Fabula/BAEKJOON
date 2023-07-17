b293a6580c282fda090778ff2c03a33938cff97a
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:24:05 +0900

    Update 직각삼각형.cc

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-int main(int avg, char** argv) {
+int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	int arr_[3] = { 0 };
