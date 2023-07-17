df0af7274ad9e6227b6de868d47f4c1bacb904db
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:26:28 +0900

    Update 나는야 포켓몬 마스터 이다솜.cc

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -5,7 +5,7 @@
 #include <cctype>
 using namespace std;
 
-int main(int avg, char** argv) {
+int main(int argc, char** argv) {
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
     int N, M; cin >> N >> M;
