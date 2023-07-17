2fe392008afa7a43633069ed1001b413ce898626
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:26:49 +0900

    Update 듣보잡.cc

--- 백준/Silver/1764. 듣보잡/듣보잡.cc
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -3,7 +3,7 @@
 #include<algorithm>
 using namespace std;
 
-int main(int avg, char** argv) {
+int main(int argc, char** argv) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     vector<string>v_Unheard, v_NotSeen;
