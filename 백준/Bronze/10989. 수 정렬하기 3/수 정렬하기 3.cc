d9c73a39b6cd4209b7ee6cab4890104678c2aff0
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:21:00 +0900

    Update 수 정렬하기 3.cc

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -2,7 +2,7 @@
 using namespace std;
 
 int arrCount[10001];
-int main() {
+int main(int argc, char **argv) {
     ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
     int repeat, num;
