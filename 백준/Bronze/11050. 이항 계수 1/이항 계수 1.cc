80b1936924084ae1d4b6b811c39739c5670d6382
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 03:39:22 +0900

    [Bronze I] Title: 이항 계수 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int factorial(int);
+int main(void) {
+    int N, k;
+    cin >> N >> k;
+    cout << factorial(N) / (factorial(k) * factorial(N - k));
+return 0; }
+
+int factorial(int N) {
+    if (N <= 1) {
+        return 1;
+    }
+    else { return N * factorial(N - 1); }
+}
