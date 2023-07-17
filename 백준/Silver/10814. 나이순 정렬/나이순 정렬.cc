e3f87bb6df4ce4d2e481db9293e267110bd172a5
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:25:16 +0900

    Update 나이순 정렬.cc

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-int main(int avg, char** argv) {
+int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL), cout.tie(NULL);
 	multimap <int, string> mm_;
