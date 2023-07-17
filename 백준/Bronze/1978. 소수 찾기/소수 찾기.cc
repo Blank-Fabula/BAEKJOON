70dc98bfdc730a9c9f19e9bf512c539ef453de0f
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:22:25 +0900

    Update 소수 찾기.cc

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int main(int avg, char** argv) {
+int main(int argc, char** argv) {
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
     int cnt = 0;
