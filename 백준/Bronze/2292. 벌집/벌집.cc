96c2436b4a0ddc1446a3b7313e2bdb07b8a2c59d
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 04:22:43 +0900

    Update 벌집.cc

--- 백준/Bronze/2292. 벌집/벌집.cc
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int main() { int N;
-	std::cin >> N;
+int main(int argc, char** argv) {
+	int N; std::cin >> N;
 	int cnt = 1;
 	for (int sum = 1; N > sum; cnt++) {
 		sum += cnt * 6;
