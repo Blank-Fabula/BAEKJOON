ff67a0b1f7b5b02e2e36077730cf40c652418cea
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-17 14:09:10 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+int main(int avg, char** argv) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int arr_[3] = { 0 };
+	while (1) {
+		cin >> arr_[0] >> arr_[1] >> arr_[2];
+		if (!arr_[0] && !arr_[1] && !arr_[2]) {
+			break;
+		} else {
+			sort(arr_, arr_ + 3);
+			if (pow(arr_[0], 2) + pow(arr_[1], 2) == pow(arr_[2], 2)) {
+				cout << "right" << '\n';
+			} else cout << "wrong" << '\n';
+		}
+	}
+return 0; }
