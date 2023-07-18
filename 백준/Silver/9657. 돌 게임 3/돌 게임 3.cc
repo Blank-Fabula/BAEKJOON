edb0e923c8d0b414348b7f5b48d3ffdf90e699df
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-18 10:54:02 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL), cout.tie(NULL);
+	int N; cin >> N;
+	if (N % 7 == 0 || N % 7 == 2) { cout << "CY";
+	} else { cout << "SK"; }
+return 0; }
