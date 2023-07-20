19928ffe0e7300e33bc4bc41d486f3cca3519d7c
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-20 20:57:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 92 ms, Memory: 6392 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> v_Graph;
+vector<bool> visit;
+int dfs(int, int);
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N, M; cin >> N >> M;
+    v_Graph.resize(N + 1);
+    visit.resize(N + 1);
+    while(M--) {
+        int u, v; cin >> u >> v;
+        v_Graph[u].push_back(v);
+        v_Graph[v].push_back(u);
+    }
+    int cnt = 0;
+    for (int i = 1; i <= N; i++) {
+        if (!visit[i]) { cnt = dfs(cnt, i); }
+    }
+    cout << cnt;
+return 0; }
+
+int dfs(int cnt, int N) {
+    visit[N] = true;
+    for (int j = 0; j < v_Graph[N].size(); j++) {
+        int next = v_Graph[N][j];
+        if (!visit[next]) { dfs(cnt, next); }
+    } cnt++;
+return cnt; }
