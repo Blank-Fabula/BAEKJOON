066d7e15447f7f3d5f6b16e4b83fdf74739c577b
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-25 00:11:29 +0900

    [Silver III] Title: 칸토어 집합, Time: 0 ms, Memory: 3612 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+#define MAX 12
+int main(int argc, char** argv) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	vector<string> v_str_Set(MAX + 1, "-");
+	for (int i = 1; i <= MAX; ++i) {
+		v_str_Set[i] = v_str_Set[i - 1] +
+			string(v_str_Set[i - 1].length(), ' ') +
+			v_str_Set[i - 1];
+	}
+	int n;
+	while (cin >> n) {
+		cout << v_str_Set[n] << "\n";
+	}
+return 0; }
