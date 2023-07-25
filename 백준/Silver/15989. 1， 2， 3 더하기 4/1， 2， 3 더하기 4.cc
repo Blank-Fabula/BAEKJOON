b59a2f34e64fd86b6acf301fc8f8cdf195957218
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-25 13:00:43 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int Dp[10'001];
+int dp[10'001];
 int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int cnt = 0, num = 6;
-	for (int i = 1; i < size(Dp); ++i) {
+	for (int i = 1; i < 10'001; ++i) {
 		if (i % num == 0) { ++cnt; }
 		else if (i % (num + 1) == 0) { --cnt; }
-		Dp[i] = Dp[i - 1] + cnt + 1;
+		dp[i] = dp[i - 1] + cnt + 1;
 		if (i % (num + 1) == 0) { ++cnt;
 			num += 6;
 		}
@@ -17,6 +17,6 @@ int main(int argc, char** argv) {
 	int T; cin >> T;
 	while (T--) {
 		int n; cin >> n;
-		cout << Dp[n] << '\n';
+		cout << dp[n] << '\n';
 	}
 return 0; }
