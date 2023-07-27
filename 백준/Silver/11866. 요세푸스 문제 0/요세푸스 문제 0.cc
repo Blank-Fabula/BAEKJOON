ee4637084e950c87c5e91ff7ab25990d77d362bb
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-27 17:30:49 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.cc
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main(int argc, char** argv) {
+	queue<int> q_JP;
+	int n, k; cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		q_JP.push(i);
+	}
+	cout << '<';
+	while (q_JP.empty() == false) {
+		for (int i = 1; i < k; i++) {
+			q_JP.push(q_JP.front());
+			q_JP.pop();
+		}
+		cout << q_JP.front();
+		q_JP.pop();
+		if (q_JP.empty() == false) { cout << ", "; }
+	}
+	cout << '>';
+return 0; }
