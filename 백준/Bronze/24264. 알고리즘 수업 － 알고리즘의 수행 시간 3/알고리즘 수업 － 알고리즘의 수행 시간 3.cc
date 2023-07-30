de2b8623d80f4d2ed4b5687846de6381051aaf6b
LEE GI TAEK <fabula.story47@gmail.com>
2023-07-30 21:52:39 +0900

    Update 알고리즘 수업 － 알고리즘의 수행 시간 3.cc

--- 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.cc
@@ -5,6 +5,6 @@ int main(int argc, char** argv){
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     long  n; cin >> n;
-    cout << n*n << '\n';
+    cout << n * n << '\n';
     cout << 2;
 return 0; }
