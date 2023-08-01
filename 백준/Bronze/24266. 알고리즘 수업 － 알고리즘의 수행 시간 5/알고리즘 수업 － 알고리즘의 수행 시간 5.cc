81c34f33c664d230e8ba3b2e858df9174553cea8
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-01 18:59:46 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    long long n; cin >> n;
+    cout << n * n * n << '\n' << 3;
+return 0; }
