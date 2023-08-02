eacdcda7e92902be5c411f6c4c5bbce80ba8c4bd
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-02 15:14:14 +0900

    Update 하노이 탑.cc

--- 백준/Gold/2270. 하노이 탑/하노이 탑.cc
+++ 백준/Gold/2270. 하노이 탑/하노이 탑.cc
@@ -7,19 +7,14 @@ int power2[100001];
 int pole[100001];
 
 void power(int&);
+void inPole();
 int hanoi(int, int);
 int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int n; cin >> n;
+    inPole();
 	power(n);
-	int p[3]; cin >> p[0] >> p[1] >> p[2];
-	for (int i = 0; i < 3; i++) {
-		while (p[i]--) {
-			int plateValue; cin >> plateValue;
-			pole[plateValue] = i + 1;
-		}
-	}
 	int result = hanoi(n, pole[n]);
 	cout << pole[n] << '\n' << result;
 return 0; }
@@ -27,6 +22,16 @@ return 0; }
 void power(int& n) { power2[0] = 1;
 	for (int i = 1; i <= n; i++) {
 		power2[i] = 2 * power2[i - 1] % DIVISOR;
+	}
+}
+
+void inPole() {
+	int p[3]; cin >> p[0] >> p[1] >> p[2];
+	for (int i = 0; i < 3; i++) {
+		while (p[i]--) {
+			int plateValue; cin >> plateValue;
+			pole[plateValue] = i + 1;
+		}
 	}
 }
 
