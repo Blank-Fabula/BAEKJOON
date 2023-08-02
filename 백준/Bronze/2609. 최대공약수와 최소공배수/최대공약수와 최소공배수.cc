4870940aaaf5bd56a148b2ef2241ca2865ac2635
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-03 01:55:11 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -1,19 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int gcd(int, int);
-int lcm(int, int);
 int main(int argc, char** argv) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	int A, B; cin >> A >> B;
-	cout << gcd(A, B) << "\n" << lcm(A, B);
+	int a, b; cin >> a >> b;
+	int result;
+	for (int i = 1; i <= a; ++i) {
+		if (a % i == 0 && b % i == 0) { result = i; }
+	}
+	cout << result << '\n' << a * b / result;
 return 0; }
-
-int gcd(int A, int B) {
-	if (B == 0) return A;
-	return gcd(B, A % B);
-}
-int lcm(int A, int B) {
-	return A * B / gcd(A, B);
-}
