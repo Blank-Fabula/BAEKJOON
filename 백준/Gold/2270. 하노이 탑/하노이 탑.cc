78d40c70288425433bed36848fd49724ae8dfc45
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-02 15:17:37 +0900

    Update 하노이 탑.cc

--- 백준/Gold/2270. 하노이 탑/하노이 탑.cc
+++ 백준/Gold/2270. 하노이 탑/하노이 탑.cc
@@ -10,29 +10,29 @@ void power(int&);
 void inPole();
 int hanoi(int, int);
 int main(int argc, char** argv) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	int n; cin >> n;
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int n; cin >> n;
     inPole();
-	power(n);
-	int result = hanoi(n, pole[n]);
-	cout << pole[n] << '\n' << result;
+    power(n);
+    int result = hanoi(n, pole[n]);
+    cout << pole[n] << '\n' << result;
 return 0; }
 
 void power(int& n) { power2[0] = 1;
-	for (int i = 1; i <= n; i++) {
-		power2[i] = 2 * power2[i - 1] % DIVISOR;
-	}
+    for (int i = 1; i <= n; i++) {
+        power2[i] = 2 * power2[i - 1] % DIVISOR;
+    }
 }
 
 void inPole() {
-	int p[3]; cin >> p[0] >> p[1] >> p[2];
-	for (int i = 0; i < 3; i++) {
-		while (p[i]--) {
-			int plateValue; cin >> plateValue;
-			pole[plateValue] = i + 1;
-		}
+    int p[3]; cin >> p[0] >> p[1] >> p[2];
+    for (int i = 0; i < 3; i++) {
+        while (p[i]--) {
+            int plateValue; cin >> plateValue;
+            pole[plateValue] = i + 1;
 	}
+    }
 }
 
 int moveCnt = 0;
@@ -41,7 +41,7 @@ int hanoi(int plate, int nextPole) {
 	int currentPlate = pole[plate];
 	int restPole = 6 - currentPlate - nextPole;
 	if (currentPlate == nextPole) { hanoi(--plate, nextPole);
-	} else { moveCnt = (moveCnt + power2[plate - 1]) % DIVISOR;
-		hanoi(--plate, restPole);
-	}
+    } else { moveCnt = (moveCnt + power2[plate - 1]) % DIVISOR;
+    hanoi(--plate, restPole);
+    }
 return moveCnt; }
