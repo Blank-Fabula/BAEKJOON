ba89268a5886979fd66da65fc99b1908282f695a
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-03 20:44:06 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#define DIVISOR 1000000000
+int main(int argc, char** argv) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int dp[100][10] = { 0 };
+	int n; cin >> n;
+	for (int i = 1; i < 10; ++i) { dp[0][i] = 1; }
+	for (int i = 1; i < n; ++i) {
+		dp[i][0] = dp[i - 1][0 + 1] % DIVISOR;
+		for (int j = 1; j <= 8; ++j) {
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % DIVISOR;
+		}
+		dp[i][9] = dp[i - 1][9 - 1] % DIVISOR;
+	}
+	int result = 0;
+	for (int i = 0; i < 10; ++i) {
+		result = (result + dp[n - 1][i]) % DIVISOR;
+	}
+	cout << result;
+return 0; }
