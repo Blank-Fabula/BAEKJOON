db063f9106d405eb60a77347e21c7935dcb293c4
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-06 00:13:08 +0900

    [Platinum III] Title: 왕들의 외나무다리 돌게임, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Platinum/18937. 왕들의 외나무다리 돌게임/왕들의 외나무다리 돌게임.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N; cin >> N;
+    int calc = 0;
+    while(N--) {
+        int A; cin >> A;
+        calc ^= A - 2;
+    }
+    string start; cin >> start;
+    if (calc != 0) { cout << start; }
+    else {
+        if (start == "Blackking") { cout << "Whiteking"; }
+        else { cout << "Blackking"; }
+    }
+return 0; }
