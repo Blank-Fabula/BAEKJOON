0c25566290207f813338ea6e563c31d759bc3e90
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-06 18:59:12 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 236 ms, Memory: 22252 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int N; cin >> N;
+    vector<int> arr_A(N), check(N), v;
+    int cnt = 0;
+    for (int i = 0; i < N; ++i) {
+        cin >> arr_A[i];
+        int value = lower_bound(v.begin(), v.end(), arr_A[i]) - v.begin();
+        check[i] = value;
+        if (value == v.size()) { ++cnt;
+            v.push_back(arr_A[i]);
+        } else { v[value] = arr_A[i]; }
+    } cout << cnt << '\n';
+    vector<int> result;
+    for (int i = N - 1; i >= 0; --i) {
+        if (cnt < 1) { break;
+        } else if (check[i] == cnt - 1) { --cnt;
+            result.push_back(arr_A[i]);
+        }
+    }
+    for (int i = result.size() - 1; i >= 0; --i) {
+        cout << result[i] << ' ';
+    }
+return 0; }
