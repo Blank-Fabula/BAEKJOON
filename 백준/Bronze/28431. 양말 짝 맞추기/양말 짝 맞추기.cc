a8c1491ebb6da3f2623568001e490ee0fc266438
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-07 17:45:49 +0900

    [Bronze IV] Title: 양말 짝 맞추기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.cc
+++ 백준/Bronze/28431. 양말 짝 맞추기/양말 짝 맞추기.cc
@@ -4,17 +4,9 @@ using namespace std;
 int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
-	int arr_A[5];
+	int sock = 0;
 	for (int i = 0; i < 5; ++i) {
-		cin >> arr_A[i];
-		for (int j = 0; j < i; ++j) {
-			if (arr_A[i] == arr_A[j]) {
-				arr_A[i] = -1;
-				arr_A[j] = -1;
-			}
-		}
-	}
-	for (int i = 0; i < 5; ++i) {
-		if (arr_A[i] != -1) { cout << arr_A[i]; }
-	}
+		int N; cin >> N;
+		sock ^= N;
+	} cout << sock;
 return 0; }
