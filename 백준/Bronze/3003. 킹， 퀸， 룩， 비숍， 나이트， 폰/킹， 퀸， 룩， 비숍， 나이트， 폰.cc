d5f7c4f4ee4245bc651ee86f47b51a340c641e07
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-09 20:37:05 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -5,12 +5,11 @@ int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	int chess[] = { 1, 1, 2, 2, 2, 8 };
-	int out[] = { 0, 0, 0, 0, 0, 0 };
+	int in[] = { 0, 0, 0, 0, 0, 0 };
 	for (int i = 0; i < 6; ++i) {
-		cin >> out[i];
-		out[i] = chess[i] - out[i];
+		cin >> in[i];
 	}
 	for (int i = 0; i < 6; ++i) {
-		cout << out[i] << ' ';
+		cout << chess[i] - in[i] << ' ';
 	}
 return 0; }
