1dbc2ca9a12ea7c84697d4b68543b2aa7e4a8d91
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-11 04:21:35 +0900

    Update 이항 계수 1.cc

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.cc
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    int N, K; cin >> N >> K;
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     int result = 1;
+    int N, K; cin >> N >> K;
     for (int i = 1; i <= K; i++) {
         result *= N - i + 1;
         result /= i;
