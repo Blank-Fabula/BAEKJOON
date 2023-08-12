cdd097b77478b919270c3ce786c64451ce3ee553
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-12 11:01:38 +0900

    Update 스택 수열.cc

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -4,8 +4,8 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
 	stack<int> s; vector<char> result;
 	int cnt = 0;
 	int N; cin >> N;
