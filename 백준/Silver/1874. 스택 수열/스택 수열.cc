c6c2b83f101efa4978cf77ec61011340dc7d7f0d
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-12 10:55:55 +0900

    [Silver II] Title: 스택 수열, Time: 20 ms, Memory: 2552 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+	stack<int> s; vector<char> result;
+	int cnt = 0;
+	int N; cin >> N;
+	while(N--) { int integer; cin >> integer;
+	while (cnt < integer) { s.push(++cnt); result.push_back('+'); }
+		if (s.top() != integer) { cout << "NO"; return 0; }
+		s.pop(); result.push_back('-');
+	}
+	for (int i = 0; i < result.size(); i++) { cout << result[i] << '\n'; }
+return 0; }
