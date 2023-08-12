aac4265361c9a37ff9966fcb89d84817698df3e4
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-12 14:04:44 +0900

    Update 가운데를 말해요.cc

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -3,21 +3,21 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-ios::sync_with_stdio(false);
-cin.tie(NULL); cout.tie(NULL);
-priority_queue<int, vector<int>, less<int>> heap_right;
-priority_queue<int, vector<int>, greater<int>> heap_left;
-int N; cin >> N;
-while (N--) {
-int integer; cin >> integer;
-heap_right.push(integer);
-while (heap_right.size() - heap_left.size() > 1) {
-heap_left.push(heap_right.top()); heap_right.pop();
-}
-if (!heap_left.empty() && heap_left.top() < heap_right.top()) {
-heap_right.push(heap_left.top()); heap_left.pop();
-heap_left.push(heap_right.top()); heap_right.pop();
-}
-cout << heap_right.top() << '\n';
-}
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    priority_queue<int, vector<int>, less<int>> heap_right;
+    priority_queue<int, vector<int>, greater<int>> heap_left;
+    int N; cin >> N;
+    while (N--) {
+        int integer; cin >> integer;
+        heap_right.push(integer);
+        while (heap_right.size() - heap_left.size() > 1) {
+            heap_left.push(heap_right.top()); heap_right.pop();
+        }
+        if (!heap_left.empty() && heap_left.top() < heap_right.top()) {
+            heap_right.push(heap_left.top()); heap_left.pop();
+            heap_left.push(heap_right.top()); heap_right.pop();
+        }
+        cout << heap_right.top() << '\n';
+    }
 return 0; }
