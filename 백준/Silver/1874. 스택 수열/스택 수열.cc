bd7c7e06ca06cc2e3dbf15efb939509119ef0973
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-12 11:02:17 +0900

    Update 스택 수열.cc

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -4,15 +4,15 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	stack<int> s; vector<char> result;
-	int cnt = 0;
-	int N; cin >> N;
-	while(N--) { int integer; cin >> integer;
-	while (cnt < integer) { s.push(++cnt); result.push_back('+'); }
-		if (s.top() != integer) { cout << "NO"; return 0; }
-		s.pop(); result.push_back('-');
-	} int size = result.size();
-	for (int i = 0; i < size; i++) { cout << result[i] << '\n'; }
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+stack<int> s; vector<char> result;
+int cnt = 0;
+int N; cin >> N;
+while(N--) { int integer; cin >> integer;
+while (cnt < integer) { s.push(++cnt); result.push_back('+'); }
+if (s.top() != integer) { cout << "NO"; return 0; }
+s.pop(); result.push_back('-');
+} int size = result.size();
+for (int i = 0; i < size; i++) { cout << result[i] << '\n'; }
 return 0; }
