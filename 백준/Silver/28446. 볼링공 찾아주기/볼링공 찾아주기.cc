3f04cb6e2ca62d0db9ee7446a6a5376d316427b1
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-13 21:17:40 +0900

    Update 볼링공 찾아주기.cc

--- 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.cc
+++ 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.cc
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <map>
-
 using namespace std;
 
 int main(int argc, char** argv) {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     map<int, int> balls;
-    int num, weight;
     int M; cin >> M;
     for (int i = 0; i < M; i++) {
         int request; cin >> request;
+        int num, weight;
         if (request == 1) { cin >> num >> weight;
             balls[weight] = num;
         } else if (request == 2) { cin >> weight;
