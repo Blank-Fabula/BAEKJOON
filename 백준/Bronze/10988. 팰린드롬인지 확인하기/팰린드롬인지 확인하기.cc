6fd5ee6f2f1beb36bd51f8ae5d988cd4c105c9ce
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-14 19:08:26 +0900

    Update 팰린드롬인지 확인하기.cc

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -6,7 +6,8 @@ int main(int argc, char** argv) {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	string word; cin >> word;
-	for (int i = 0; (i * 2) < word.size(); i++) {
-		if (word[i] != word[word.size() - 1 - i]) { cout << 0; return 0; }
+	int size = word.size();
+	for (int i = 0; i <= size / 2; i++) {
+		if (word[i] != word[size - 1 - i]) { cout << 0; return 0; }
 	} cout << 1;
 return 0; }
