b6ee917fc849bfee21d38807ada2b8cfe62434e8
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-15 04:54:51 +0900

    Update 숨바꼭질.cc

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -8,22 +8,23 @@ int main(int argc, char** argv) {
     cin.tie(NULL); cout.tie(NULL);
     queue<pair<int, int>> q_;
     int N, K; cin >> N >> K;
-    vector<bool> visited((N > K ? N : K) + 2 + 1);
-    int result = 0;
-    q_.push(make_pair(N, result));
-    while (1) {
-        int location = q_.front().first;
-        result = q_.front().second;
-        q_.pop();
-        if (location == K) { break; }
-        if (location + 1 <= K && !visited[location + 1]) { q_.push(make_pair(location + 1, result + 1));
+    int location = N, result = 0;
+    vector<bool> visited((N > K ? N : K) + 2 + 1, 0);
+    visited[N] = true;
+    for (; location != K;) {
+        if (location + 1 <= K && !visited[location + 1]) {
+            q_.push(make_pair(location + 1, result + 1));
             visited[location + 1] = true;
         }
-        if (location - 1 >= 0 && !visited[location - 1]) { q_.push(make_pair(location - 1, result + 1));
+        if (location - 1 >= 0 && !visited[location - 1]) {
+            q_.push(make_pair(location - 1, result + 1));
             visited[location - 1] = true;
         }
-        if (location * 2 <= K + 2 && !visited[location * 2]) { q_.push(make_pair(location * 2, result + 1));
+        if (location * 2 <= K + 2 && !visited[location * 2]) {
+            q_.push(make_pair(location * 2, result + 1));
             visited[location * 2] = true;
         }
+        location = q_.front().first, result = q_.front().second;
+        q_.pop();
     } cout << result;
 return 0; }
