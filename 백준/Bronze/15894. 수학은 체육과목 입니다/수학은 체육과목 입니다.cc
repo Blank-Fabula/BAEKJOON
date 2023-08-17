2c9e7b93b3ab0dad367ce38e2e42f6ddd9f35c32
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-17 10:21:03 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    unsigned int N; cin >> N;
+    cout << N * 4;
+return 0; }
