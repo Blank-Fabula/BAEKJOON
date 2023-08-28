e6ba534acc466b577f4766e57b64c08566516ef3
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-28 12:09:24 +0900

    [Silver III] Title: 소수 구하기, Time: 192 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int&);
+int main(int argc, char** argv) {
+	int M, N; cin >> M >> N;
+	for (int i = M; i <= N; i++) {
+		if (isPrime(i)) { cout << i << '\n'; }
+	}
+return 0; }
+
+bool isPrime(int& num) {
+	if (num < 2) return false;
+	for (int i = 2; i * i <= num; i++) {
+		if (num % i == 0) { return false; }
+	}
+return true; }
