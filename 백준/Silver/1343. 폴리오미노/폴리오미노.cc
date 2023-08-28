086422c0635a13f16ae2e4892254f0019e168aa8
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-29 07:10:05 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -1,21 +1,11 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
 using namespace std;
 
 int main(int argc, char** argv) {
-	bool isTrue = true;
-	int cnt = 0;
-	string str; cin >> str;
-	for (int i = 0; i < str.size(); i++) { char c = str[i];
-		if (c != '.' && i == str.size() - 1) { cnt++, i++; }
-		if (c == '.' || i == str.size()) {
-			if (cnt % 2 != 0) { isTrue = false; break;
-			} else {
-				for (; cnt - 4 >= 0; cnt -= 4) { replace(str.begin() + i - cnt, str.begin() + i - cnt + 4, 'X', 'A'); }
-				for (; cnt - 2 >= 0; cnt -= 2) { replace(str.begin() + i - cnt, str.begin() + i - cnt + 2, 'X', 'B'); }
-			} cnt = 0;
-		} else { cnt++; }
-	} if (isTrue) { cout << str;
-	} else { cout << -1; }
+    string str; cin >> str;
+    for (int pos = 0; (pos = str.find("XXXX", pos)) >= 0; ) { str.replace(pos, 4, "AAAA"); }
+    for (int pos = 0; (pos = str.find("XX", pos)) >= 0; ) { str.replace(pos, 2, "BB"); }
+    if (str.find("X", 0) == string::npos) { cout << str;
+    } else { cout << -1; }
 return 0; }
