44eae3474a7a0ef106fd19d12c233c9d18c3c052
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-28 12:10:08 +0900

    Update 소수 구하기.cc

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -10,7 +10,7 @@ int main(int argc, char** argv) {
 return 0; }
 
 bool isPrime(int& num) {
-	if (num < 2) return false;
+	if (num < 2) { return false; }
 	for (int i = 2; i * i <= num; i++) {
 		if (num % i == 0) { return false; }
 	}
