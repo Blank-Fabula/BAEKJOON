1a9b827c64d4c062b145e6abadfbb58e0a5d17d0
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-29 18:17:22 +0900

    Update 통계학.cc

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,35 +1,31 @@
 #include <iostream>
-#include <set>
 #include <vector>
 #include <algorithm>
+#include <set>
 #include <cmath>
 using namespace std;
 
-struct answer{
-	double sum = 0;
-	int maxCnt = 0;
-	set<int> maxNums;
-};
 int main(int argc, char** argv) {
-	answer result;
 	int numCnt[8001] = { 0 };
+	double sum = 0;
 	int N; cin >> N;
 	vector<int> nums(N);
 	for (int i = 0; i < N; i++) {
 		cin >> nums[i];
-		result.sum += nums[i];
 		numCnt[nums[i] + 4000]++;
+		sum += nums[i];
 	} sort(nums.begin(), nums.end());
-	for (int i = 0; i < N; i++) {
-		if (numCnt[nums[i] + 4000] >= result.maxCnt) {
-			if (numCnt[nums[i] + 4000] > result.maxCnt) { result.maxNums.clear(); }
-			result.maxNums.insert(nums[i]);
-			result.maxCnt = numCnt[nums[i] + 4000];
+	set<int> maxNums;
+	for (int maxCnt = 0, i = 0; i < N; i++) {
+		if (numCnt[nums[i] + 4000] > maxCnt) { maxNums.clear(); }
+		if (numCnt[nums[i] + 4000] >= maxCnt) {
+			maxNums.insert(nums[i]);
+			maxCnt = numCnt[nums[i] + 4000];
 		}
 	}
-	cout << (int)round(result.sum / N) << '\n';
+	cout << (int)round(sum / N) << '\n';
 	cout << nums[N / 2] << '\n';
-	if (result.maxNums.size() > 1) { result.maxNums.erase(*result.maxNums.begin()); }
-	cout << *result.maxNums.begin() << '\n';
+	if (maxNums.size() > 1) { maxNums.erase(*maxNums.begin()); }
+	cout << *maxNums.begin() << '\n';
 	cout << nums.back() - nums.front();
 return 0; }
