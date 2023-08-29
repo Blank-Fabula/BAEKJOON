980795f3539a953f33c47f6858994e7a88be2fe3
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-30 07:37:45 +0900

    Update 통계학.cc

--- 백준/Silver/2108. 통계학/통계학.cc
+++ 백준/Silver/2108. 통계학/통계학.cc
@@ -1,28 +1,29 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 #include <cmath>
 using namespace std;
 
 int main(int argc, char** argv) {
     ios::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-	int numsCnt[8001] = { 0 };
+	int numCnt[8001] = { 0 };
 	double sum = 0;
 	int N; cin >> N;
 	vector<int> nums(N);
 	for (int i = 0; i < N; i++) {
 		cin >> nums[i];
-		numsCnt[nums[i] + 4000]++;
+		numCnt[nums[i] + 4000]++;
 		sum += nums[i];
 	} sort(nums.begin(), nums.end());
-	vector<int> frequent;
-    for (int maxCnt = 0, i = nums.front(); i <= nums.back(); i++) {
-        if (numsCnt[i + 4000] > maxCnt) { frequent.clear(); maxCnt = numsCnt[i + 4000]; }
-		if (numsCnt[i + 4000] >= maxCnt) { frequent.push_back(i); }
-	} if (frequent.size() > 1) { sort(frequent.begin(), frequent.end()); frequent.erase(frequent.begin()); }
+	set<int> maxNums;
+	for (int maxCnt = 0, i = 0; i < N; i++) {
+		if (numCnt[nums[i] + 4000] > maxCnt) { maxNums.clear(); maxCnt = numCnt[nums[i] + 4000]; }
+		if (numCnt[nums[i] + 4000] >= maxCnt) { maxNums.insert(nums[i]); }
+	} if (maxNums.size() > 1) { maxNums.erase(*maxNums.begin()); }
 	cout << (int)round(sum / N) << '\n';
 	cout << nums[N / 2] << '\n';
-	cout << *frequent.begin() << '\n';
+	cout << *maxNums.begin() << '\n';
 	cout << nums.back() - nums.front();
 return 0; }
