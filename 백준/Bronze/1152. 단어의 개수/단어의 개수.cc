a29ecbed477e758aad5d9a9f4eafc1f9600d447c
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-30 11:06:41 +0900

    [Bronze II] Title: 단어의 개수, Time: 8 ms, Memory: 4148 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -3,13 +3,12 @@
 #include <string>
 using namespace std;
 
-vector<char> s;
 int main(int argc, char** argv) {
     ios::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	string str; getline(cin, str);
 	if (str.size() == 0) { cout << 0; return 0; }
-	for (char c : str) { s.push_back(c); }
+	vector<char> s(str.begin(), str.end());
 	if (s.size() && s.front() == ' ') { s.erase(s.begin()); }
 	if (s.size() && s.back() == ' ') { s.resize(s.size() - 1); }
 	int result = 0;
