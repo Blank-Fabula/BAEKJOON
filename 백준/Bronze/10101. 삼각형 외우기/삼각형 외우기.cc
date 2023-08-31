ad262b56ddb055822ef41b77088ab0c347c5ed13
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-01 06:20:55 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int A, B, C; cin >> A >> B >> C;
+    if (A + B + C != 180) { cout << "Error\n";
+    } else if (A == B && B == C) { cout << "Equilateral\n";
+    } else if (A == B || A == C || B == C) { cout << "Isosceles\n";
+    } else { cout << "Scalene\n"; }
+return 0; }
