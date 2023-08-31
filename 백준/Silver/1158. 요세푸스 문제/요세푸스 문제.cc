4ca3c3e9dd6b6b02ac7622d86adf223acbc91b5c
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-31 16:55:11 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+	int N, K; cin >> N >> K;
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++) { arr[i] = i + 1; }
+	cout << "<";
+	for (int i = K - 1; arr.size() > 1; i += (K - 1)) {
+		if (i + 1 > arr.size()) { i %= arr.size(); }
+		cout << arr[i] << ", ";
+		for (int j = i; j < arr.size() - 1; j++) { arr[j] = arr[j + 1]; }
+		arr.erase(arr.end() - 1);
+	} cout << arr[0] << ">";
+return 0; }
