a8165f416492cad112f7ef52241f02c0b7cf5114
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-31 22:07:36 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 40 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.cc
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 	int TC; cin >> TC; cin.ignore();
 	while (TC--) { int start = 0;
 		string str; getline(cin, str); str += " ";
