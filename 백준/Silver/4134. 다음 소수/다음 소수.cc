954e84269e75c2dcc73148d1531463d4644ef712
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-02 17:25:59 +0900

    [Silver IV] Title: 다음 소수, Time: 296 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool isPrime(long long&);
+int main(int argc, char** argv) {
+	int TC; std::cin >> TC;
+	while (TC--) {
+		long long N; std::cin >> N;
+		for (; !isPrime(N); N++);
+		std::cout << N << "\n";
+	}
+return 0; }
+
+bool isPrime(long long& N) {
+	if (N < 2) { return false; }
+	for (long long i = 2; i * i <= N; i++) {
+		if (N % i == 0) { return false; }
+	}
+return true; }
