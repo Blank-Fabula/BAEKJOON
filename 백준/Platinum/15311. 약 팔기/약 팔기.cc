717461c85378fe721da38754b78c77f5c04e6b95
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-09 10:06:19 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Platinum/15311. 약 팔기/약 팔기.cc
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -2,7 +2,7 @@
 
 int main(int argc, char** argv) {
     std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr); std::cout.tie(nullptr);
+    std::cin.tie(nullptr);
     std::cout << 2000 << "\n";
     for (int i = 0; i < 1000; i++) { std::cout << 1 << " "; }
     for (int i = 0; i < 1000; i++) { std::cout << 1000 << " "; }
