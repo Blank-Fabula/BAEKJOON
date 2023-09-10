87b1fcfacd051b5303202c5e0a7fb8abf5828de6
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-10 11:49:49 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main(int argc, char** argv) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    queue<pair<int, int>> q_;
+    int N, K; cin >> N >> K;
+    int location = N, result = 0;
+    vector<bool> visited((N > K ? N : K) + 2 + 1, 0);
+    visited[N] = true;
+    for (; location != K;) {
+        if (location * 2 <= K + 2 && !visited[location * 2]) {
+            q_.push(make_pair(location * 2, result));
+            visited[location * 2] = true;
+        }
+        if (location - 1 >= 0 && !visited[location - 1]) {
+            q_.push(make_pair(location - 1, result + 1));
+            visited[location - 1] = true;
+        }
+        if (location + 1 <= K && !visited[location + 1]) {
+            q_.push(make_pair(location + 1, result + 1));
+            visited[location + 1] = true;
+        }
+        location = q_.front().first, result = q_.front().second;
+        q_.pop();
+    } cout << result;
+return 0; }
