579bc0c383681f12687141ffc986f060c650eaa6
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-10 21:46:06 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.cc
+++ 백준/Silver/1074. Z/Z.cc
@@ -1,27 +1,20 @@
 #include <iostream>
-using namespace std;
 
-void index();
-void z(int, int, int);
-int main(int argc, char** argv) {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	int N; cin >> N;
-	index();
-	z(1 << N, 0, 0);
-return 0; }
-
-int r, c;
-void index() { cin >> r >> c; }
-
-int cnt = 0;
+int r, c, cnt = 0;
 void z(int size, int y, int x) {
-	if (r == y && c == x) { cout << cnt; return; }
-	if ((r >= y && r < y + size) && (c >= x && c < x + size)) {
+	if (r == y && c == x) { std::cout << cnt;
+    } else if ((r >= y && r < y + size) && (c >= x && c < x + size)) {
 		size /= 2;
 		z(size, y, x);
         z(size, y, x + size);
 		z(size, y + size, x);
 		z(size, y + size, x + size);
 	} else { cnt += size * size; }
-}
+return; }
+int main(int argc, char** argv) {
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(nullptr); std::cout.tie(nullptr);
+	int N; std::cin >> N;
+	std::cin >> r >> c;
+	z(1 << N, 0, 0);
+return 0; }
