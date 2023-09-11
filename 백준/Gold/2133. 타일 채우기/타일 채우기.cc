70e6ed0bc1f78f6493536b17283c04ac0ab2aa72
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-11 17:14:45 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
 	int N; std::cin >> N;
 	if (N % 2 == 0) {
 		for (int i = 4; i <= N; i += 2) {
-			dp[i] = 3 * dp[i - 2] + dp[i - 2] - dp[i - 4];
+			dp[i] = 4 * dp[i - 2] - dp[i - 4];
 		} result = dp[N];
 	}
 	std::cout << result;
