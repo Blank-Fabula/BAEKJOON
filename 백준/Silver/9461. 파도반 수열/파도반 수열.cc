e597b057acfa678dfb66e9f07eee4840f5448ca4
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-16 00:11:34 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -2,13 +2,15 @@
 typedef long long ll;
 
 int main(int argc, char* argv[]) {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 	ll dp[100] = { 1, 1, 1 };
 	for (int idx = 3; idx < 100; idx++) {
 		dp[idx] = dp[idx - 2] + dp[idx - 3];
 	}
 	int testCase; std::cin >> testCase;
-	while (testCase--) {
-		int N; std::cin >> N;
-		std::cout << dp[N - 1] << "\n";
+	for (int num; testCase--; ) {
+        std::cin >> num;
+		std::cout << dp[num - 1] << "\n";
 	}
 return 0; }
