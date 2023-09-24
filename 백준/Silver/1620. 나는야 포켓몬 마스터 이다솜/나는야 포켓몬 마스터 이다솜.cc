d9de2ac254ac9c1940d2f3203c9537ace7a06042
LEE GI TAEK <fabula.story47@gmail.com>
2023-09-24 23:38:46 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 152 ms, Memory: 20864 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -5,22 +5,26 @@
 #include <cctype>
 using namespace std;
 
-int main(int argc, char** argv) {
+int main(int avg, char** argv) {
     ios::sync_with_stdio(false);
     cin.tie(NULL), cout.tie(NULL);
     int N, M; cin >> N >> M;
     map<string, int> m_;
     vector<string> v_s_Name(N + 1);
     for (int i = 1; i <= N; i++) {
-        cin >> v_s_Name[i];
-        m_.insert(make_pair(v_s_Name[i], i));
+        string s_; cin >> s_;
+        v_s_Name[i] = s_;
+        m_.insert(make_pair(s_, i));
     }
     while (M--) {
         string s_; cin >> s_;
+        int num;
         if (isdigit(s_[0])) {
-            cout << v_s_Name[stoi(s_)] << '\n';
+            num = stoi(s_);
+            cout << v_s_Name[num] << '\n';
         } else {
-            cout << m_[s_] << '\n';
+            auto it = m_.find(s_);
+            cout << it->second << '\n';
         }
     }
 return 0; }
