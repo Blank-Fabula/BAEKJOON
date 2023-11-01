bae101a166a9fa5f7bac834b5f7939ba351d3308
LEE GI TAEK <fabula.story47@gmail.com>
2023-11-02 02:40:05 +0900

    [Bronze II] Title: 문자열 분석, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.c
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 int main(int argc, char* argv[]) {
 	int a, b, c, d, i;
 	char str[101];
-	for (; gets(str); ) {
-		a = 0, b = 0, c = 0, d = 0;
+	for (; gets(str); ) { a = 0, b = 0, c = 0, d = 0;
 		for (i = 0; i < strlen(str); i++) {
 			if (islower(str[i])) { a++; }
 			else if (isupper(str[i])) { b++; }
 			else if (isdigit(str[i])) { c++; }
-			else if (str[i] == ' ') { d++; }
+			else if (isspace(str[i])) { d++; }
 		} printf("%d %d %d %d\n", a, b, c, d);
 	}
 return 0; }
