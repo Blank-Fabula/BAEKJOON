add9df0d0a10bce9f835e76d3366e5201df47eb2
LEE GI TAEK <fabula.story47@gmail.com>
2023-11-20 00:04:05 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -10,10 +10,6 @@ int main(int argc, char *argv[]) {
 	std::string N; std::cin >> N;
 	int B; std::cin >> B;
 
-	int answer = 0, N_len = N.length();
-	for (int i = N_len; i > 0; i--) {
-		if (isdigit(N[i - 1])) { answer += (N[i - 1] - '0') * ((int)pow(B, N_len - i)); }
-		else { answer += (N[i - 1] - 'A' + 10) * ((int)pow(B, N_len - i)); }
-	} std::cout << answer << '\n';
+	std::cout << std::strtol(N.c_str(), NULL, B);
 
 return 0; }
