34258ce452cfd462f225350d08dfd83c6e0cd187
LEE GI TAEK <fabula.story47@gmail.com>
2024-02-14 20:12:11 +0900

    [Bronze III] Title: 진주로 가자! (Easy), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze III/31009. 진주로 가자！ （Easy）/진주로 가자！ （Easy）.cc
@@ -0,0 +1,51 @@
+/*making - Blank Fabula
+title - Baekjoon Problem.30969
+date - 2024.02.14*/
+
+#include <iostream>
+#include <array>
+#include <string>
+
+#define targetTerminal "jinju"
+int main(int argc, char *argv[]) 
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    std::pair<int, int> answer = { 0, 0 };
+
+    int N;
+    std::cin >> N;
+
+    std::array<int, 1001> tolls = { 0 };
+    std::pair<std::string, long long> terminal;
+    while (N--)
+    {
+        std::cin >> terminal.first >> terminal.second;
+
+        if (terminal.first == targetTerminal)
+        {
+            answer.first = terminal.second;
+        }
+        else if (terminal.second > 1000)
+        {
+            answer.second++;
+        }
+        else
+        {
+            tolls[terminal.second]++;
+        }
+    }
+
+    for (int index = 2; index < tolls.size(); index++)
+    {
+        if (index > answer.first)
+        {
+            answer.second += tolls[index];
+        }
+    }
+
+    std::cout << answer.first << '\n' << answer.second;
+
+    return 0;
+}
