40d870b3e9cc981d61965bf4e562ab4adef7278e
LEE GI TAEK <fabula.story47@gmail.com>
2024-04-21 13:10:15 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main (int argc, char *argv[])
+{
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    
+    int N;
+    std::cin >> N;
+
+    std::cout << (N & 1 ? "evoLmoCicS" : "SciComLove") << '\n';
+    
+    return 0;
+}
