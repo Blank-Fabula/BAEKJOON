5a0289c2c28f307022115f035348eed2d6ac5c88
LEE GI TAEK <fabula.story47@gmail.com>
2024-05-02 03:02:09 +0900

    [Silver II] Title: 회문수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/30446. 회문수/회문수.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+typedef unsigned long long int ull;
+
+int main(int argc, char **argv)
+{
+  std::cin.tie(0)->sync_with_stdio(0);
+
+  ull n;
+  std::cin >> n;
+
+  ull calc, cnt = 0;
+  for (int i = 1; i < 100000; ++i)
+  {
+    calc = i;
+    for (int j = i * 10; j /= 10;)
+    {
+      calc = (calc * 10) + (j % 10);
+    }
+    calc <= n ? ++cnt : cnt;
+
+    calc = i;
+    for (int j = i; j /= 10;)
+    {
+      calc = (calc * 10) + (j % 10);
+    }
+    calc <= n ? ++cnt : cnt;
+  }
+
+  std::cout << cnt;
+
+  return 0;
+}
