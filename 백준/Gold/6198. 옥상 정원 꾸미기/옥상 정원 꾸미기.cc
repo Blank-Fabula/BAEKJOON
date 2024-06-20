eb705cf3e0ad3e14490e47d6d4a434e3cb9d8031
LEE GI TAEK <fabula.story47@gmail.com>
2024-06-20 16:15:21 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 12 ms, Memory: 2680 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -1,18 +1,21 @@
 #include <iostream>
 #include <stack>
+
 typedef long long ll;
 
 int main(int argc, char* argv[]) {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr); std::cout.tie(nullptr);
-	std::stack<ll> st;
-	ll answer = 0;
+
+	ll answer = 0, H;
+    
 	int N; std::cin >> N;
-	for (int i = 0; i < N; i++) {
-		ll H; std::cin >> H;
+	for (std::stack<ll> st; N--; st.push(H)) {
+		std::cin >> H;
 		while (!st.empty() && st.top() <= H) { st.pop(); }
 		answer += st.size();
-		st.push(H);
 	}
-	std::cout << answer;
+    
+    std::cout << answer;
+    
 return 0; }
