014f0871680ecd4e3b596003b7c760fb0f752a20
LEE GI TAEK <fabula.story47@gmail.com>
2024-06-21 01:41:47 +0900

    [Platinum V] Title: 히스토그램, Time: 8 ms, Memory: 2940 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char **argv)
+{
+  std::cin.tie(0)->sync_with_stdio(0);
+
+  int ans = 0;
+  
+  int n; std::cin >> n;
+  std::vector<int> arr(++n + 1);
+
+  for (int i = 1; i < n; ++i) { std::cin >> arr[i]; }
+
+  std::stack<int> st;
+  st.push(0);
+  for (int i = 1; i <= n; ++i)
+  {
+    while (!st.empty() && arr[st.top()] > arr[i])
+    {
+      int tmp = st.top();
+      st.pop();
+      ans = std::max(ans, arr[tmp] * (i - st.top() - 1));
+    }
+    st.push(i);
+  }
+
+  std::cout << ans;
+
+  return 0;
+}
